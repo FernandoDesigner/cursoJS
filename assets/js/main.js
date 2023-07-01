faa43d8fd5e762f7a428ab9d2de47ab1088901ae
Fernando <fernando.designer1999@gmail.com>
2023-06-30 22:22:49 -0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// let num1 = parseInt(prompt("ingresa el numero 1"))
+// let num2 = parseInt(prompt("ingresa el numero 2"))
+// let resul = num1 + num2
+// console.log(resul)
+
+// let opcionUsuario = prompt(`
+
+//     1: console
+//     2: log
+// `);
+
+// switch (opcionUsuario) {
+//   case "1":
+//     console.log("hola");
+//     break;
+//   case "2":
+//     console.log("ayola");
+//     break;
+//   default:
+//     console.log("opcioon no valida");
+//     break;
+// }
+
+// let numero = 1
+// while(numero <= 10){
+//     console.log(numero);
+//         //numero = numero + 1;
+//         numero ++ 
+// }
+// console.log("while termino en el numero " +  numero)
+var o = {p: 21, q: 13};
+var {p,q} = o;
+console.log(p+q)
